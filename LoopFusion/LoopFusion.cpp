52b6ac6075c95bdaf5d73b6397ff3ebf57d3d9e2
ilija-s <ilijastc062@gmail.com>
2023-07-17 21:01:31 +0200

    Improve: Defined new types and removed FindFusionCandidates method

--- LoopFusion/LoopFusion.cpp
+++ LoopFusion/LoopFusion.cpp
@@ -1,25 +1,30 @@
+#include "FusionCandidate.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/LoopNestAnalysis.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
 using namespace llvm;
 
 namespace {
-  struct LoopFusion : public FunctionPass {
-    std::vector<std::set<Loop *>> FusionCandidates; // Might not be the most suitable data stucture, as Loop equivalence comparison is currently questionable.
-  
-    static char ID; // Pass identification, replacement for typeid
-    LoopFusion() : FunctionPass(ID) {}
-    
-    std::vector<std::set<Loop *>> FindFusionCandidates(Function *F) { // Function that will fill and sort the FusionCandidates data structure.
-    	std::vector<std::set<Loop *>> placeholder;
-    	return placeholder;
-    }
-    
-    bool CanFuseLoops(Loop *L1, Loop *L2) { // Do all checks to figure out if loops can be fused.
-    	return true;
-    }
-    
-    void FuseLoops(Loop *L1, Loop *L2); // Function that will fuse loops based on previously established candidates.
+
+using FusionCandidatesTy = SmallVector<FusionCandidate>;
+// TODO: Implement sorting of FusionCandidates that are CFE, they need to be
+// ordered in respect to their dominance order.
+using CFESetsTy = SmallVector<std::set<FusionCandidate>>;
+
+struct LoopFusion : public FunctionPass {
+  FusionCandidatesTy FusionCandidates;
+  CFESetsTy CFESets;
+  static char ID; // Pass identification, replacement for typeid
+
+  LoopFusion() : FunctionPass(ID) {}
+
+  /// Do all checks to figure out if loops can be fused.
+  bool CanFuseLoops(Loop *L1, Loop *L2) { return true; }
+
+  /// Function that will fuse loops based on previously established candidates.
+  void FuseLoops(Loop *L1, Loop *L2);
 
   bool runOnFunction(Function &F) override {
     // for each loop L: LoopInfo analysis pass is needed
