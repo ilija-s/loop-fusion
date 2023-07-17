b616b6b4cd70dd8ae7fd5361dfaa2693d7f2e587
ilija-s <ilijastc062@gmail.com>
2023-07-17 20:59:15 +0200

    New: Added FusionCandidate class representing a single candidate for fusion

---
+++ LoopFusion/FusionCandidate.h
@@ -0,0 +1,17 @@
+#ifndef LIB_FUSIONCANDIDATE_H
+#define LIB_FUSIONCANDIDATE_H
+
+/// Class used to represent a fusion candidate.
+class FusionCandidate {
+public:
+  FusionCandidate(Loop *L) : L(L){};
+
+  /// Checks if a loop is a candidate for a loop fusion.
+  auto isCandidateForFusion() const -> bool;
+
+private:
+  // Loop that represents a fusion candidate
+  Loop *L;
+};
+
+#endif // LIB_FUSIONCANDIDATE_H
