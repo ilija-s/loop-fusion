a2782388bf3e14873dc1f807eea1aecc41001e75
ilija-s <ilijastc062@gmail.com>
2023-07-18 11:58:22 +0200

    New: Added LoopInfo and SCEV analysis passes that will be used for loop fusion

--- LoopFusion/LoopFusion.cpp
+++ LoopFusion/LoopFusion.cpp
@@ -1,6 +1,7 @@
 #include "FusionCandidate.h"
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Analysis/LoopNestAnalysis.h"
+#include "llvm/Analysis/ScalarEvolution.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
@@ -26,6 +27,14 @@ struct LoopFusion : public FunctionPass {
   /// Function that will fuse loops based on previously established candidates.
   void FuseLoops(Loop *L1, Loop *L2);
 
+  void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<ScalarEvolutionWrapperPass>();
+    AU.addRequired<LoopInfoWrapperPass>();
+
+    AU.addPreserved<ScalarEvolutionWrapperPass>();
+    AU.addPreserved<LoopInfoWrapperPass>();
+  }
+
   bool runOnFunction(Function &F) override {
     // for each loop L: LoopInfo analysis pass is needed
     //    collect fusion candidates - Use FusionCandidate class to determine
@@ -34,6 +43,18 @@ struct LoopFusion : public FunctionPass {
     //      for each pair of loops Li Lj
     //        if (CanFuseLoops(Li, Lj)):
     //          FuseLoops(Li, Lj)
+
+    auto &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
+    auto &SE = getAnalysis<ScalarEvolutionWrapperPass>().getSE();
+
+    SmallVector<Loop *> FunctionLoops(LI.begin(), LI.end());
+    for (const auto &L : FunctionLoops) {
+      dbgs() << "Number of basic blocks: " << L->getNumBlocks() << "\n";
+    }
+
+    dbgs() << "Total number of loop basic blocks in a function: "
+           << FunctionLoops.capacity() << "\n";
+
     return true;
   }
 };
