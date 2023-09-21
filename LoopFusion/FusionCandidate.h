97ce4ac547170577fa6e9b581417449ec44b3686
ajili <ilijastc062p@gmail.com>
2023-09-21 21:44:09 +0200

    New: Added methods for retrieving loop preheader, header, exiting block and latch

--- LoopFusion/FusionCandidate.h
+++ LoopFusion/FusionCandidate.h
@@ -8,16 +8,29 @@ using namespace llvm;
 /// Class used to represent a fusion candidate.
 class FusionCandidate {
 public:
-  FusionCandidate(Loop *L) : L(L){};
+  FusionCandidate(Loop *L) : L(L) {
+    Preheader = L->getLoopPreheader();
+    Header = L->getHeader();
+    ExitingBlock = L->getExitingBlock();
+    Latch = L->getLoopLatch();
+  };
 
   /// Checks if a loop is a candidate for a loop fusion.
   auto isCandidateForFusion() const -> bool;
 
-  Loop* getLoop();
+  Loop *getLoop();
+  inline BasicBlock *getPreheader() { return Preheader; };
+  inline BasicBlock *getHeader() { return Header; };
+  inline BasicBlock *getExitingBlock() { return ExitingBlock; };
+  inline BasicBlock *getLatch() { return Latch; };
 
 private:
   // Loop that represents a fusion candidate
   Loop *L;
+  BasicBlock *Preheader;
+  BasicBlock *Header;
+  BasicBlock *ExitingBlock;
+  BasicBlock *Latch;
 };
 
 #endif // LIB_FUSIONCANDIDATE_H
